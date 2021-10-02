14899d62cec8781e12a9a5c1e0fbf36b97205653
itchief <alex.malcev1980@gmail.com>
2021-10-02 12:42:32 +1000

    Update

---
+++ toast/js/toast.js
@@ -0,0 +1,59 @@
+/**
+ * Toast (https://github.com/itchief/ui-components/tree/master/toast)
+ * Copyright 2020 - 2021 Alexander Maltsev
+ * Licensed under MIT (https://github.com/itchief/ui-components/blob/master/LICENSE)
+**/
+
+class Toast {
+  constructor(params) {
+    this._title = params['title'] === false ? false : params['title'] || 'Title';
+    console.log(this._title);
+    this._text = params['text'] || 'Message...';
+    this._theme = params['theme'] || 'default';
+    this._autohide = params['autohide'] && true;
+    this._interval = +params['interval'] || 5000;
+    console.log(this._title);
+    this._create();
+    this._el.addEventListener('click', (e) => {
+      if (e.target.classList.contains('toast__close')) {
+        this._hide();
+      }
+    });
+    this._show();
+  }
+  _show() {
+    this._el.classList.add('toast_show');
+    if (this._autohide) {
+      setTimeout(() => {
+        this._hide();
+      }, this._interval);
+    }
+  }
+  _hide() {
+    this._el.classList.remove('toast_show');
+    const event = new CustomEvent('hide.toast', { detail: { target: this._el } });
+    document.dispatchEvent(event);
+  }
+  _create() {
+    const el = document.createElement('div');
+    el.classList.add('toast');
+    el.classList.add(`toast_${this._theme}`);
+    let html = `{header}<div class="toast__body"></div><button class="toast__close" type="button"></button>`;
+    const htmlHeader = this._title === false ? '' : '<div class="toast__header"></div>';
+    html = html.replace('{header}', htmlHeader);
+    el.innerHTML = html;
+    if (this._title) {
+      el.querySelector('.toast__header').textContent = this._title;
+    } else {
+      el.classList.add('toast_message');
+    }
+    el.querySelector('.toast__body').textContent = this._text;
+    this._el = el;
+    if (!document.querySelector('.toast-container')) {
+      const container = document.createElement('div');
+      container.classList.add('toast-container');
+      document.body.append(container);
+    }
+    document.querySelector('.toast-container').append(this._el);
+  }
+}
