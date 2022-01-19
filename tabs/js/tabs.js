47554e7858ae2fc02d2993bb8ed936830277e977
itchief <alex.malcev1980@gmail.com>
2022-01-19 21:07:25 +1000

    Update

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -3,32 +3,41 @@ class ItcTabs {
     const defaultConfig = {};
     this._config = Object.assign(defaultConfig, config);
     this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
-    this._elLinks = this._elTabs.querySelectorAll('.tabs__link');
+    this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
     this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
-    this._eventShow = new Event('tab.itc.show');
+    this._eventShow = new Event('tab.itc.change');
+    this._init();
     this._events();
   }
+  _init() {
+    this._elTabs.setAttribute('role', 'tablist');
+    this._elButtons.forEach((el, index) => {
+      el.dataset.index = index;
+      el.setAttribute('role', 'tab');
+      this._elPanes[index].setAttribute('role', 'tabpanel');
+    });
+  }
   show(elLinkTarget) {
-    const href = elLinkTarget.getAttribute('href');
-    const elPaneTarget = document.querySelector(href);
-    const elLinkActive = this._elTabs.querySelector('.tabs__link_active');
+    const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+    const elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
     const elPaneShow = this._elTabs.querySelector('.tabs__pane_show');
     if (elLinkTarget === elLinkActive) {
       return;
     }
-    elLinkActive ? elLinkActive.classList.remove('tabs__link_active') : null;
+    elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
     elPaneShow ? elPaneShow.classList.remove('tabs__pane_show') : null;
-    elLinkTarget.classList.add('tabs__link_active');
+    elLinkTarget.classList.add('tabs__btn_active');
     elPaneTarget.classList.add('tabs__pane_show');
     this._elTabs.dispatchEvent(this._eventShow);
+    elLinkTarget.focus();
   }
   showByIndex(index) {
-    const elLinkTarget = this._elLinks[index];
+    const elLinkTarget = this._elButtons[index];
     elLinkTarget ? this.show(elLinkTarget) : null;
   };
   _events() {
     this._elTabs.addEventListener('click', (e) => {
-      const target = e.target.closest('.tabs__link');
+      const target = e.target.closest('.tabs__btn');
       if (target) {
         e.preventDefault();
         this.show(target);
