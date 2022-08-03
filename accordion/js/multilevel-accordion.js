d58bb8c0e2a9fa9d88a9a804e498f539e30e479f
itchief <alex.malcev1980@gmail.com>
2022-08-03 20:21:01 +1000

    Update

---
+++ accordion/js/multilevel-accordion.js
@@ -0,0 +1,79 @@
+class ItcAccordion {
+  constructor(target, config) {
+    this._el = typeof target === 'string' ? document.querySelector(target) : target;
+    const defaultConfig = {
+      alwaysOpen: true,
+      duration: 350
+    };
+    this._config = Object.assign(defaultConfig, config);
+    this.addEventListener();
+  }
+  addEventListener() {
+    this._el.addEventListener('click', (e) => {
+      const elHeader = e.target.closest('.accordion__header');
+      if (!elHeader) {
+        return;
+      }
+      if (!this._config.alwaysOpen) {
+        this._el.querySelectorAll('.accordion__item_show').forEach((el) => {
+          if (el.contains(elHeader.parentElement)) {
+            return;
+          }
+          el !== elHeader.parentElement ? this.toggle(el) : null;
+        });
+      }
+      this.toggle(elHeader.parentElement);
+    });
+  }
+  show(el) {
+    const elBody = el.querySelector('.accordion__body');
+    if (elBody.classList.contains('collapsing') || el.classList.contains('accordion__item_show')) {
+      return;
+    }
+    elBody.style.display = 'block';
+    const height = elBody.offsetHeight;
+    elBody.style.height = 0;
+    elBody.style.overflow = 'hidden';
+    elBody.style.transition = `height ${this._config.duration}ms ease`;
+    elBody.classList.add('collapsing');
+    el.classList.add('accordion__item_slidedown');
+    elBody.offsetHeight;
+    elBody.style.height = `${height}px`;
+    window.setTimeout(() => {
+      elBody.classList.remove('collapsing');
+      el.classList.remove('accordion__item_slidedown');
+      elBody.classList.add('collapse');
+      el.classList.add('accordion__item_show');
+      elBody.style.display = '';
+      elBody.style.height = '';
+      elBody.style.transition = '';
+      elBody.style.overflow = '';
+    }, this._config.duration);
+  }
+  hide(el) {
+    const elBody = el.querySelector('.accordion__body');
+    if (elBody.classList.contains('collapsing') || !el.classList.contains('accordion__item_show')) {
+      return;
+    }
+    elBody.style.height = `${elBody.offsetHeight}px`;
+    elBody.offsetHeight;
+    elBody.style.display = 'block';
+    elBody.style.height = 0;
+    elBody.style.overflow = 'hidden';
+    elBody.style.transition = `height ${this._config.duration}ms ease`;
+    elBody.classList.remove('collapse');
+    el.classList.remove('accordion__item_show');
+    elBody.classList.add('collapsing');
+    window.setTimeout(() => {
+      elBody.classList.remove('collapsing');
+      elBody.classList.add('collapse');
+      elBody.style.display = '';
+      elBody.style.height = '';
+      elBody.style.transition = '';
+      elBody.style.overflow = '';
+    }, this._config.duration);
+  }
+  toggle(el) {
+    el.classList.contains('accordion__item_show') ? this.hide(el) : this.show(el);
+  }
+}
