63979d1e6835b9ef56a9c75f457661ca01d64aac
itchief <alex.malcev1980@gmail.comm>
2022-09-29 21:47:26 +1000

    Update

---
+++ btn-up/btn-up.js
@@ -0,0 +1,49 @@
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    if (this.el.classList.contains('btn-up_hide')) {
+      this.el.classList.remove('btn-up_hide');
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  hide() {
+    if (!this.el.classList.contains('btn-up_hide')) {
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.add('btn-up_hide');
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  addEventListener() {
+    // при прокрутке окна (window)
+    window.addEventListener('scroll', () => {
+      // если пользователь прокрутил страницу более чем на 200px
+      if ((window.scrollY || document.documentElement.scrollTop) > 400) {
+        // сделаем кнопку .btn-up видимой
+        this.show();
+      } else {
+        // иначе скроем кнопку .btn-up
+        this.hide();
+      }
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      this.hide();
+      // переместиться в верхнюю часть страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
+
+
+
